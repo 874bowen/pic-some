4fee5acd6ccb1e40ba697f9762d284b5b06a09ca
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-05-15 10:30:22 +0300

    add outlet to render the children

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Header from './components/Header'
 import './App.css'
+import { Outlet } from "react-router-dom"
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Home Page</h1>
+      <Outlet />
     </div>
   )
 }
