56c48795a6aa18bcabf772f975e45b45a58610bc
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-05-15 09:18:02 +0300

    initial project set-up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default App
