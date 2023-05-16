a55379c5c7f597959831eec5b6d70820e1978a5f
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-05-16 13:00:14 +0300

    use useContext to read values passed by provider

--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -1,9 +1,32 @@
+import { useContext } from "react";
+import { Context } from "../context/context";
+import Image from "../components/Image";
+import { getClass } from "../utils";
+
+interface Photo {
+    url: string;
+    id: string;
+    isFavorite: boolean;
+}
+ 
 function Photos() {
-   return (
-       <main className="photos">
-           <h1>Images go here</h1>
-       </main>
-   )
+    const context = useContext(Context)
+    const {allPhotos}: any = context
+    console.log(allPhotos);
+
+    const images = allPhotos.map((photo: Photo) => {
+        const className = getClass(Number(photo.id))
+        return(
+            <Image url={photo.url} className={className} />
+        )
+    })
+
+	return (
+		<main className="photos">
+			<h1>Images go here</h1>
+            {images}
+		</main>
+	);
 }
 
-export default Photos
+export default Photos;
