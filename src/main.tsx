01b174adc1bbefc83c23288033c28921fad25cb4
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-05-16 13:01:25 +0300

    configure context provider for app

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Photos from './pages/Photos.tsx'
-import Cart from './pages/Cart.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Photos from "./pages/Photos.tsx";
+import Cart from "./pages/Cart.tsx";
+import { ContextProvider } from "./context/context.tsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    children: [
-      {
-        path: "",
-        element: <Photos />,
-      },
-      {
-        path: "cart",
-        element: <Cart />,
-      }
-    ],
-  },
-])
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "",
+				element: <Photos />,
+			},
+			{
+				path: "cart",
+				element: <Cart />,
+			},
+		],
+	},
+]);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<ContextProvider>
+		<React.StrictMode>
+			<RouterProvider router={router} />
+		</React.StrictMode>
+	</ContextProvider>
+);
