225ee16f23cac9b5a79e87b6bdbfcb5ac5677a71
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-05-17 16:23:34 +0300

    update context to add items to cart

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -6,12 +6,13 @@ interface Photo {
    isFavorite: boolean;
 }
 
-const Context = React.createContext<{allPhotos: Photo[], toggleFavorite: (id:string) => void} | undefined>(undefined)
+const Context = React.createContext<{allPhotos: Photo[], cartItems: Photo[], toggleFavorite: (id:string) => void, addToCart: (newItem: Photo) => void, removeFromCart: (id:string) => void, setCartItems: (value: any) => void} | undefined>(undefined)
 
 const {Provider} = Context
 
 function ContextProvider({children}: any) {
    const [allPhotos, setAllPhotos] = useState<Photo[]>([])
+   const [cartItems, setCartItems] = useState<Photo[]>([])
 
    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
 
@@ -28,14 +29,24 @@ function ContextProvider({children}: any) {
       })
    }
 
+   const addToCart = (newItem: Photo) => {
+      setCartItems(prev => [...prev, newItem])
+   }
+
+   const removeFromCart = (id: string) => {
+      setCartItems(prev => prev.filter(item => item.id !== id))
+   }
+
    useEffect(() => {
       fetch(url).then(res => res.json()).then(res => {
          setAllPhotos(res)
       })
    }, [])
    
+   console.log(cartItems)
+
    return (
-      <Provider value={{allPhotos, toggleFavorite}}>
+      <Provider value={{allPhotos, cartItems, toggleFavorite, addToCart, removeFromCart, setCartItems}}>
          {children}
       </Provider>
    )
