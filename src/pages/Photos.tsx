e39233aab9442b75133242c73f3c67ebd5deab8f
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-05-17 11:48:50 +0300

    display images

--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { Context } from "../context/context";
 import Image from "../components/Image";
-import { getClass } from "../utils";
 
 interface Photo {
     url: string;
@@ -15,15 +14,13 @@ function Photos() {
     console.log(allPhotos);
 
     const images = allPhotos.map((photo: Photo) => {
-        const className = getClass(Number(photo.id))
         return(
-            <Image url={photo.url} className={className} />
+            <Image key={photo.id} img={photo} />
         )
     })
 
 	return (
 		<main className="photos">
-			<h1>Images go here</h1>
             {images}
 		</main>
 	);
