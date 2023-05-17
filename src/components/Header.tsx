acb1c0857aa165a62233303b111dc5dd11aad373
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-05-17 16:19:51 +0300

    style cart conditionally

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
 import { Link } from 'react-router-dom'
+import { useContext } from "react";
+import { Context } from "../context/context";
+
 function Header() {
+    const {cartItems}: any = useContext(Context)
+    const cartClassName = cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
     return (
         <header>
             <Link to="/"><h2>Pic Some</h2></Link>
-            <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+            <Link to="/cart" style={{textDecoration: 'none'}}><i className={`${cartClassName} ri-fw ri-2x`}></i></Link>
         </header>
     )
 }
