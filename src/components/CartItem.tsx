2bd8ede5da53f0e9024b61b33ee635af34758dfb
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-05-17 15:01:03 +0300

    add cart Item component

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+function CartItem({item, removeFromCart}: any) {
+    return (
+        <div className="cart-item">
+            <i className="ri-delete-bin-line" onClick={() => removeFromCart(item.id)}></i>
+            <img src={item.url} width="130px" />
+            <p>$5.99</p>
+        </div>
+    )
+}
+
+export default CartItem
