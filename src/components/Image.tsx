ff51d6a5e779bbd6ba0e971734c488020a699197
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-05-17 16:20:35 +0300

    render icons

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -7,17 +7,34 @@ import { getClass } from "../utils";
 function Image({ img  }: any) {
 	const context = useContext(Context)
 	const [hovered, setHovered] = useState(false);
-	const {toggleFavorite}: any = context
+	const {toggleFavorite, addToCart, cartItems, removeFromCart}: any = context
 
-	// <i className="ri-heart-fill favorite"></i>
-	const heartIcon = hovered && <i onClick={() => toggleFavorite(img.id)} className={`${img.isFavorite ? 'ri-heart-fill': 'ri-heart-line'} favorite`}></i>;
-	const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
+	const isInCart = (id: string) => {
+		return cartItems.some((item: any) => item.id === id);
+	};
+
+	function heartIcon() {
+		if (img.isFavorite) {
+			return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>;
+		} else if (hovered) {
+			return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>;
+		}
+	}
+
+	function cartIcon() {
+		if (isInCart(img.id)) {
+			console.log("in cart");
+			return <i className="ri-shopping-cart-fill cart"  onClick={() => removeFromCart(img.id)}></i>;
+		} else if (hovered) {
+			return <i className="ri-add-circle-line cart"  onClick={() => addToCart(img)}></i>;
+		}
+	}
 	
 	return (
 		<div className={`${getClass(Number(img.id))} image-container`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
 			<img src={img.url} className="image-grid" />
-			{heartIcon}
-			{cartIcon}
+			{heartIcon()}
+			{cartIcon()}
 		</div>
 	);
 }
