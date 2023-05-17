a5bcbe50743fa33cbb0363952155c16782d351e7
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-05-17 16:23:55 +0300

    add cart component

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,41 @@
+import {useContext, useEffect, useState} from "react"
+import {Context} from "../context/context"
+import CartItem from "../components/CartItem"
+
 function Cart() {
-   return (
-       <main className="cart-page">
-           <h1>Check out</h1>
-       </main>
-   )
+    const {cartItems, removeFromCart, setCartItems}: any = useContext(Context)
+    const [isOrdering, setIsOrdering]: any = useState(false)
+    const [totalCost, setTotalCost] = useState((0).toLocaleString("en-US", {style: "currency", currency: "USD"}))
+    const cartItemElements = cartItems.map((item: any) => (
+        <CartItem key={item.id} item={item} removeFromCart={removeFromCart} />
+    ))
+    
+    function placeOrder() {
+        setIsOrdering(true)
+        setTimeout(() => {
+            console.log("Order placed!")
+            setCartItems([])
+            setIsOrdering(false)
+        } ,3000)
+    }
+    useEffect(() => {
+        setTotalCost((5.99 * cartItems.length).toLocaleString("en-US", {style: "currency", currency: "USD"}))
+    }, [cartItems])
+
+    return (
+        <main className="cart-page">
+            <h1>Check out</h1>
+            {cartItemElements}
+            <p className="total-cost">Total: {totalCost}</p>
+            {
+                cartItems.length > 0 ?
+                <div className="order-button">
+                    <button onClick={placeOrder}>{ isOrdering ? 'Ordering...': 'Place Order'}</button>
+                </div> :
+                <p>You have no items in your cart.</p>
+            }
+        </main>
+    )
 }
 
 export default Cart
