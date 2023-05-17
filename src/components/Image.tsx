43d15a1960c4e03c6891a2c83f14dc086653923f
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-05-17 11:47:15 +0300

    impelement context consumer

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,11 +1,25 @@
-import React from "react"
+import {useState} from "react";
+import { useContext } from "react";
+import { Context } from "../context/context";
+import { getClass } from "../utils";
 
-function Image({className, url}: any) {
-    return (
-        <div className={`${className} image-container`}>
-            <img src={url} className="image-grid"/>
-        </div>
-    )
+
+function Image({ img  }: any) {
+	const context = useContext(Context)
+	const [hovered, setHovered] = useState(false);
+	const {toggleFavorite}: any = context
+
+	// <i className="ri-heart-fill favorite"></i>
+	const heartIcon = hovered && <i onClick={() => toggleFavorite(img.id)} className={`${img.isFavorite ? 'ri-heart-fill': 'ri-heart-line'} favorite`}></i>;
+	const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
+	
+	return (
+		<div className={`${getClass(Number(img.id))} image-container`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+			<img src={img.url} className="image-grid" />
+			{heartIcon}
+			{cartIcon}
+		</div>
+	);
 }
 
-export default Image
+export default Image;
